f8a1f202099edc7e4672b30922334c02c6d89882
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-23 09:50:00 -0500

    edited in class

--- js/drawing.js
+++ js/drawing.js
@@ -1,17 +1,18 @@
 enableDrawing();
 
 function enableDrawing() {
-    document.addEventListener('click', function(e) {
-        var red = Math.ceil((Math.random() * 255) + 0);
-        var green = Math.ceil((Math.random() * 255) + 0);
-        var blue = Math.ceil((Math.random() * 255) + 0);
+    document.addEventListener('mousemove', function(e) {
+        var red = Math.ceil(Math.random() * 255);
+        var green = Math.ceil(Math.random() * 255);
+        var blue = Math.ceil(Math.random() * 255);
         var box = document.createElement('div');
         box.style.position = 'absolute';
         box.style.top = e.clientY + 'px';
         box.style.left = e.clientX + 'px';
         box.style.width = '10px';
         box.style.height = '10px';
-        box.style.backgroundColor = 'rgb(255,182,193)';
+        box.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
         document.body.appendChild(box);
     });
