e12463b3ac212a7d91ca05e5a6745f0e47fa577b
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-22 21:09:10 -0500

    finished

--- js/drawing.js
+++ js/drawing.js
@@ -1,17 +1,17 @@
 enableDrawing();
 
 function enableDrawing() {
-    document.addEventListener(___________, function(e) {
-        var red = _________________________;
-        var green = _________________________;
-        var blue = _________________________;
+    document.addEventListener('click', function(e) {
+        var red = Math.ceil((Math.random() * 255) + 0);
+        var green = Math.ceil((Math.random() * 255) + 0);
+        var blue = Math.ceil((Math.random() * 255) + 0);
         var box = document.createElement('div');
         box.style.position = 'absolute';
-        box.style.top = e.__________ + 'px';
-        box.style.left = e.__________ + 'px';
+        box.style.top = e.clientY + 'px';
+        box.style.left = e.clientX + 'px';
         box.style.width = '10px';
         box.style.height = '10px';
-        box.style.backgroundColor = 'rgb(255, 0, 0)';
+        box.style.backgroundColor = 'rgb(255,182,193)';
 
         document.body.appendChild(box);
     });
